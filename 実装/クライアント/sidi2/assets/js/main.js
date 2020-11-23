12f29382403342616e9892bb02f1d3ce64369dd6
18jz0127 ハン <hangtrand6qt1@gmail.com>
2020-11-23 19:58:53 +0900

    login

---
+++ 実装/クライアント/sidi2/assets/js/main.js
@@ -0,0 +1,49 @@
+import Vue from './vendor/vue.esm.browser.js';
+import VueRouter from './vendor/vue-router.esm.browser.js';
+// views
+import Login from './views/Login.js';
+import Register from './views/Register.js';
+
+
+/*
+------- 作るページ -------
+/ Login.js
+
+*/
+
+(function() {
+    "use strict";
+
+    // VueRouter //=====//
+    Vue.use(VueRouter);
+
+    const routes = [
+        {
+            path: "/login",
+            name: "Login",
+            component: Login
+        },
+        { 
+            path: '/main', 
+            name: 'Main', 
+            query: { auth: '' }, 
+            component: Main 
+        },
+        { 
+            path: '/register', 
+            name: 'Register', 
+            component: Register 
+        }
+    ];
+
+    const router = new VueRouter({
+        routes
+    });
+
+    // vueの設定 //=====//
+    new Vue({
+        el: '#app',
+        router
+    });
+
+})();
