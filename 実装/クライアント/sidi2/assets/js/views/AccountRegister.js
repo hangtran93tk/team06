86b4184621c4e2fa60b695d1b8ff52c5ee7b5df6
18jz0127 ハン <hangtrand6qt1@gmail.com>
2020-12-07 13:54:21 +0900

    accountRegister modal

--- 実装/クライアント/sidi2/assets/js/views/AccountRegister.js
+++ 実装/クライアント/sidi2/assets/js/views/AccountRegister.js
@@ -1,99 +1,146 @@
 import Ajax from '../lib/Ajax.js';
-import User from '../lib/User';
+// import User from '../lib/User';
 
 export default {
     //テンプレート//
-    template: `<header role="banner">
-    <h1>アカウント登録</h1>
-    <link rel="stylesheet" href="./assets/css/accountRegister.css">
-</header>
+    template: `
+    <div id="input">
+        <header role="banner">
+            <h1>アカウント登録</h1>
+            <link rel="stylesheet" href="./assets/css/accountRegister.css">
+        </header>
 
-<main role="main">
-    <section id="inputInfo">
-            <input type="text" id="nickname" value="ニックネーム" v-model="user.name"><br>								
-            <input type="text" id="mail" value="メールアドレス" v-model="user.email"><br>
-            <input type="text" id="password" value="パスワード" v-model="user.password"><br>
-            <input type="text" id="passwordConfirm" value="パスワード再入力" v-model="confirm_pass">
-            <p v-if="user.password !== confirm_pass">パスワードが再入力してください</p>			
-    </section>
-    
-    <section id="birthday">
-        <h2>生年月日</h2>
-        <input type="date" name="birthday" v-model="user.birthday">
-    </section>
-
-    <div >
-        <section id="gender">
-            <h2>性別</h2>
-            <input type="radio" id="male" name="gender" value="male" v-model="picked.gender">
-            <label for="0">男性</label>
+        <main role="main">
+            <section id="inputInfo">
+                <input type="text" id="nickname" placeholder="ニックネーム" v-model="name"><br>								
+                <input type="text" class="mail" v-model="email"  placeholder="メールアドレス"><br>
+                <input type="password" id="password" placeholder="パスワード" v-model="password"><br>
+                <input type="password" id="passwordConfirm" placeholder="パスワード再入力" v-model="confirm_pass">
+                <p v-if="password !== confirm_pass">パスワードが再入力してください</p>			
+            </section>
+            <section id="birthday">
+                <h2>生年月日</h2>
+                <input type="date" id="birthday" name="birthday" v-model="birthday">
+            </section>
+            <div >
+                <section id="gender">
+                    <h2>性別</h2>
+                    <label><input type="radio"  name="gender" value="1" v-model="gender">男性</label>
+                    <label><input type="radio"  name="gender" value="2" v-model="gender">女性</label>
+                </section>
+            	
+                <div id="heightWeight">
+                    <div id="height">
+                        <h2>身長</h2>
+                        <input type="text" name="userHeight" v-model="height" ><label for="height">cm</label>
+                    </div>
+                    <div id="weight">
+                        <h2>体重</h2>
+                        <input type="text" name="userWeight" v-model="weight"><label for="weight">kg</label>
+                        <h2 class="goalWeight">目標体重</h2>
+                        <input type="text" name="userWeight" v-model="goal_weight"><label for="weight">kg</label>
+                    </div> 
+                </div>
+            </div>    
+            <section id="active">
+                <h2>運動レベル</h2>
+                <input type="radio" id="active" name="active" value="1" v-model="active" checked>
+                <label class="active" for="0">レベル１</label>
             
-            <input type="radio" id="female" name="gender" value="female" v-model="picked.gender">
-            <label for="1">女性</label>
-           
-        </section>	
-        <div id="heightWeight">
-            <div id="height">
-                <h2>身長</h2>
-                <input type="text" name="userHeight"><label for="height" v-model="user.height">cm</label>
+                <input type="radio" id="active" name="active" value="2" v-model="active">
+                <label class="active" for="1">レベル2</label>
+
+                <input type="radio" id="active" name="active" value="3" v-model="active">
+                <label class="active" for="2">レベル3</label>
+            </section>
+            <br>
+            <details>
+            <summary>運動レベルとは？</summary>
+                <img src="./assets/img/Activelevel.png" />
+            </details>
+            <br>
+
+            <div id="register">
+                <button type="button" @click="clickRegister"><label for="register">登録</label></button>
             </div>
-            <div id="weight">
-                <h2>体重</h2>
-                <input type="text" name="userWeight" v-model="user.weight"><label for="weight">kg</label>
-                <h2 class="goalWeight">目標体重</h2>
-                <input type="text" name="userWeight" v-model="user.goal_weight"><label for="weight">kg</label>
-                
-            </div> 
-            
+        </main>
+        <div class="modal" v-if="regis_error">
+            <div class="wrap">
+                <p>{{ message }}</p>
+                <button class="show-modal-btn" @click="regis_error = false">OK</button>
+            </div>
+        </div>
+        <div class="modal" v-if="regis_comp">
+            <div class="wrap">
+                <p>{{ message }}</p>
+                <router-link to="/" class="show-modal-btn">TOPへ戻る</router-link>
+            </div>
         </div>
     </div>
-    
-    
-    <section id="active">
-        <h2>運動レベル</h2>
-            
-            <input type="radio" id="active" name="active" value="1" v-model="picked.active">
-            <label class="active" for="0">レベル１</label>
-            
-            <input type="radio" id="active" name="active" value="2" v-model="picked.active">
-            <label class="active" for="1">レベル2</label>
-
-            <input type="radio" id="active" name="active" value="3" v-model="picked.active">
-            <label class="active" for="2">レベル3</label>
-    </section>
-
-    <a href="#"><h4>運動レベルとは ?</h4></a>	
-
-    <div id="register">
-        <button type="button" @click="clickRegister"><label for="register">登録</label></button>
-    </div>
-</main>`,
+    `,
+    data() {
+        return{
+            error: "",
+            email: "",
+            name: "",
+            password: "",
+            height: "",
+            goal_weight: "",   
+            birthday: "",
+            active_level: "",
+            confirm_pass: "",
+            gender: "",
+            weight: "",
+            active: "",
+            regis_comp: false,
+            regis_error: false
+        };
+    },
     methods: {
         clickActiveLevel(level) {
             let v = level;
         },
         clickRegister() {
             const obj1 = {
-                "name": this.user.name,
-                "email": this.user.email,
-                "password": this.user.password,
-                "birthday":this.user.birthday,
-                "sex": this.picked.gender,
-                "height": this.user.height,
-                "goal_weight":this.user.goal_weight,       
-                "active_level":this.picked.active
+                "email": this.email,
+                "username": this.name,
+                "password": this.password,
+                "height": this.height,
+                "goal_weight":this.goal_weight,
+                "sex": this.gender,       
+                "birthday":this.birthday,
+                "active_level":this.active
             };
             
             
 
             const obj2 = {
-                "weight": this.user.weight
+                "weight": this.weight
             };
 
-            Ajax(`http://192.168.1.10:8000/auth/user-weight/`,'POST', obj2)
+            Ajax(`http://192.168.1.10:8000/auth/register/`,'POST',null, obj1)
             .then((res) => {
                 console.log(res);
+                
+                if(res.data) {
+                    this.message = '登録完了。メール確認してください。';
+                    this.regis_comp = true;
 
+                } else {
+                    this.regis_error = true;
+                    if(res.errors.email && res.errors.password) {
+                        this.message = res.errors.email + " " + res.errors.password;
+                    }
+                    else if(res.errors.password) {
+                        this.message = res.errors.password;
+                    }
+                    else if (res.errors.email) {
+                        this.message = res.errors.email;
+                    }
+                    else{
+                        this.message = 'エラー';
+                    }                                        
+                }
             })
             .catch((err) => {
                 console.log(err);
